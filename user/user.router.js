d74cfa4c382ba70f3a0a1baa6c6c289843935459
AndryaThr <andryatsitsl@gmail.com>
2022-06-15 14:56:46 +0300

    rectification kelly clarkson

--- user/user.router.js
+++ user/user.router.js
@@ -3,16 +3,14 @@ const router = express.Router();
 const userController = require('./user.controller');
 // const testController = require('./user.controller');
 
-router.post('/signup/', userController.createNewUser);
+router.post('/signup', userController.createNewUser);
 router.get('/user/:id', userController.getUser);
 router.put('/user/:id', userController.putUser);
-router.post('/login/', userController.login);
+router.post('/login', userController.login);
 router.put('/pay/', userController.pay);
 
 // router.post('/signup/', (req, res) => {
 //     testController.createNewUser(req, res);
 // });
-
-
 
 module.exports = router;
